000f7fc87eb69f7668cd68a18fc3223da1c04ccb
jubairJnu <jubair2810@gmail.com>
2024-12-12 23:11:00 +0600

    finished

--- src/app/modules/user/user.routes.js
+++ src/app/modules/user/user.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/", auth, UsersControllers.getAllUsers);
 router.post("/register", UsersControllers.createUser);
-router.patch("/", auth, UsersControllers.updateAllUsers);
+router.patch("/:id", auth, UsersControllers.updateAllUsers);
 
 module.exports = {
   UserRouters: router,
